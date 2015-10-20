600667dc52bea48ec2a4053b6e31ec2348fff904
Ollie Relph <ollie@relph.me>
2015-10-20 21:35:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Code to accompany a blog post: [A Smarter `Promise.all()` for JavaScript (ES2015)](http://ollie.relph.me/blog/a-smarter-promise-all/)
+
+
+# Installing
+
+`npm i`
+
+# Running for Dev
+
+Run `npm run watch` in one terminal session
+
+Run `npm start` in another
---
+++ src/index.js
@@ -0,0 +1,68 @@
+import Xray from 'x-ray';
+import fs from 'fs';
+
+const xray = Xray();
+
+const urlsToGet = fs.readFileSync('./topsites.txt').toString('utf8').split(/\r?\n|\r/).slice(0, 10);
+
+const urlPromiseSequence = promiseSeq(urlsToGet, ( url, ix ) => {
+  return xrayPromise(`http://${ url }`, 'h1')
+  .catch(( err ) => {
+    // We want errors in this dataset to fail silently
+    // this is because some sites may only accept https
+    // or perhaps be down.
+    // Normally you would want to handle this
+    return null;
+  });
+});
+
+urlPromiseSequence
+.then(console.log.bind(console))
+.catch(( err ) => console.error(`ERROR: ${ err.message }\n${ err.stack }`));
+
+//
+// Helpers
+//
+
+// Wrap the standard xray function so that it returns a promise
+function xrayPromise(...args) {
+  return new Promise(( resolve, reject ) => {
+    xray(...args)(( err, obj ) => {
+      if ( err ) {
+        return reject(err);
+      }
+      return resolve(obj);
+    });
+  });
+}
+
+// split the array into smaller arrays,
+// process each of the smaller arrays before moving
+// onto the next
+function promiseSeq( arr, predicate, consecutive=10 ) {
+  return chunkArray(arr, consecutive).reduce(( prom, items, ix ) => {
+    // wait for the previous Promise.all() to resolve
+    return prom.then(( results ) => {
+      return Promise.all(
+        items.map(( item ) => {
+          return predicate(item, ix)
+          .then(( result ) => {
+            results.push(result);
+          });
+        })
+      ).then(() => results);
+    });
+  }, Promise.resolve([]));
+
+  function chunkArray( startArray, chunkSize ) {
+    let j = -1;
+    return startArray.reduce(( arr, item, ix ) => {
+      j += ix % chunkSize === 0 ? 1 : 0;
+      arr[ j ] = [
+        ...( arr[ j ] || []),
+        item,
+      ];
+      return arr;
+    }, []);
+  }
+}
